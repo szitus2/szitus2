324e1198f66c8f967b46c6b6d650bcecdbca579a
szitus2 <szitus2@gmail.com>
2019-02-24 13:56:40 +0100

    Valami

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -37,7 +37,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(203, 92);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Master";
+            this.button1.Text = "Valami";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
